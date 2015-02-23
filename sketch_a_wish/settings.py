fd886afb4f7890bfabb9695cc8411fb8446327b8
Ketan Bhatt <ktbt10@gmail.com>
2015-02-23 18:14:15 +0530

    Migrated from SQLite to PostgreSQL

--- sketch_a_wish/settings.py
+++ sketch_a_wish/settings.py
@@ -62,8 +62,12 @@ WSGI_APPLICATION = 'sketch_a_wish.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'saw',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
