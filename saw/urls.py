39cc2ad59cdd95e054bea3925939535de95773d6
Ketan Bhatt <ktbt10@gmail.com>
2015-02-23 21:25:36 +0530

    Corrected URL fetching, deleted old code

--- saw/urls.py
+++ saw/urls.py
@@ -6,7 +6,4 @@ urlpatterns = patterns('',
                        url(r'^get_started/$', views.get_started, name='get_started'),
                        url(r'^start/$', views.start, name='start'),
                        url(r'^logout/$', views.user_logout, name='logout'),
-                       url(r'^add_wish/$', views.add_wish, name='add_wish'),
-                       url(r'^get_wish/$', views.get_wish, name='get_wish'),
-                       url(r'^add_sketch/$', views.add_sketch, name='add_sketch')
             )
--- saw/views.py
+++ saw/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib import messages
+from django.core.urlresolvers import reverse
+
 
 # Create your views here.
 def sketchawish(request):
@@ -12,6 +14,8 @@ def sketchawish(request):
 
 
 def get_started(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('start'))
 
     if request.method == 'POST':
 
@@ -35,7 +39,7 @@ def get_started(request):
                 profile.save()
                 user = authenticate(username=registering_username, password=registering_password)
                 login(request, user)
-                return HttpResponseRedirect('/sketchawish/start/')
+                return HttpResponseRedirect(reverse('start'))
 
             else:
                 print user_form.errors, profile_form.errors
@@ -49,7 +53,7 @@ def get_started(request):
             if user:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponseRedirect('/sketchawish/start/')
+                    return HttpResponseRedirect(reverse('start'))
 
                 else:
                     return HttpResponse("Your account is deactivated")
@@ -70,7 +74,7 @@ def get_started(request):
 def user_logout(request):
     logout(request)
 
-    return HttpResponseRedirect('/sketchawish/')
+    return HttpResponseRedirect(reverse('sketchawish'))
 
 
 @login_required
@@ -95,7 +99,7 @@ def start(request):
                 curr_user.progress = 2
                 curr_user.save()
 
-                return HttpResponseRedirect('/sketchawish/start/')
+                return HttpResponseRedirect(reverse('start'))
 
             else:
                 print wish_form.errors
@@ -113,7 +117,7 @@ def start(request):
                 curr_user.progress = 3
                 curr_user.save()
 
-                return HttpResponseRedirect('/sketchawish/start/')
+                return HttpResponseRedirect(reverse('start'))
 
             else:
                 print get_wish_form.errors
@@ -131,7 +135,7 @@ def start(request):
                 curr_user.progress = 1
                 curr_user.save()
 
-                return HttpResponseRedirect('/sketchawish')
+                return HttpResponseRedirect(reverse('sketchawish'))
 
             else:
                 print sketch_form.errors
@@ -142,92 +146,3 @@ def start(request):
         sketch_form = SketchForm(request=request)
 
     return render(request, 'saw/start.html', {'wish_form': wish_form, 'get_wish_form': get_wish_form, 'sketch_form': sketch_form, 'progress': progress})
-
-
-@login_required
-def add_wish(request):
-
-    curr_user = UserProfile.objects.get(user = request.user)
-    if curr_user.total_wished >= curr_user.total_sketched :
-        messages.info(request, 'You need to sketch first')
-        return HttpResponseRedirect('/sketchawish/get_wish/')
-
-    if request.method == "POST":
-        wish_form = WishForm(request.POST)
-
-        if wish_form.is_valid():
-            wish = wish_form.save(commit=False)
-            wish.wisher = request.user
-            wish.save()
-
-            curr_user = UserProfile.objects.get(user = request.user)
-            curr_user.total_wished+=1
-            curr_user.save()
-
-            return sketchawish(request)
-
-        else:
-            print wish_form.errors
-
-    else:
-        wish_form = WishForm()
-
-    return render(request, 'saw/add_wish.html', {'wish_form': wish_form})
-
-@login_required
-def get_wish(request):
-
-    wishes_to_get = Wish.objects.exclude(wisher = request.user).filter(pk__in = Wish.objects.filter(locked=False)[:3].values_list('pk'))
-    if wishes_to_get.count() < 1 :
-        messages.info(request, 'Sorry there are no available wishes at the moment!')
-        return HttpResponseRedirect('/sketchawish/')
-
-    if request.method == "POST":
-        get_wish_form = GetWishForm(request.POST, request=request)
-
-        if get_wish_form.is_valid():
-            to_sketch = get_wish_form.save(commit=False)
-            to_sketch.sketcher = request.user
-            to_sketch.save()
-            Wish.objects.filter(pk=request.POST['wish']).update(locked=True, sketcher=request.user)
-
-            return sketchawish(request)
-
-        else:
-            print get_wish_form.errors
-
-    else:
-        get_wish_form = GetWishForm(request=request)
-
-    return render(request, 'saw/get_wish.html', {'get_wish_form': get_wish_form})
-
-@login_required
-def add_sketch(request):
-
-    wishes_to_sketch = Wish.objects.filter(sketcher=request.user).filter(sketched=False)
-    if wishes_to_sketch.count() < 1 :
-        messages.info(request, 'You need to select a wish first')
-        return HttpResponseRedirect('/sketchawish/get_wish/')
-
-    if request.method == "POST":
-
-        needed_pk = Sketch.objects.get(wish = request.POST['wish']).pk
-
-        sketch_form = SketchForm(request.POST, request=request, instance = Sketch.objects.get(pk=needed_pk))
-
-        if sketch_form.is_valid():
-            sketch_form.save()
-            Wish.objects.filter(pk=request.POST['wish']).update(sketched=True)
-            curr_user = UserProfile.objects.get(user = request.user)
-            curr_user.total_sketched+=1
-            curr_user.save()
-
-            return sketchawish(request)
-
-        else:
-            print sketch_form.errors
-
-    else:
-        sketch_form = SketchForm(request=request)
-
-    return render(request, 'saw/add_sketch.html', {'sketch_form': sketch_form})
